e4a37377730e25ce100e760a1042007c86553b34
fernandocandeiatorres <fernandotorrescandeia@gmail.com>
2022-07-07 21:58:01 -0300

    integrated frontend and backend on one server, connected both on the package.json outside, added logger

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,9 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
+
+const morgan = require("morgan");
+const rfs = require("rotating-file-stream");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -13,7 +17,30 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+
+// rotating write stream
+const accessLogStream = rfs.createStream("access.log", {
+  interval: "1d", // rotate daily
+  path: path.join(__dirname, "logs"),
+});
+
+// setup the logger
+app.use(
+  morgan("short", {
+    skip: function (req, res) {
+      return req.path !== "/";
+    },
+    stream: accessLogStream,
+  })
+);
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use("/planets", planetsRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
